2331aa65c2a75d60e95af19ecc35f0a7563bd555
Sereivoan Yong <sereivoanyong@gmail.com>
2025-01-04 01:05:35 +0700

    Override dark appearance

--- Sources/HXPhotoPicker/Core/Controller/HXBaseViewController.swift
+++ Sources/HXPhotoPicker/Core/Controller/HXBaseViewController.swift
@@ -7,7 +7,68 @@
 
 import UIKit
 
+open class HXBaseNavigationController: UINavigationController {
+    
+    public override init(navigationBarClass: AnyClass?, toolbarClass: AnyClass?) {
+        super.init(navigationBarClass: navigationBarClass, toolbarClass: toolbarClass)
+        commonInit()
+    }
+    
+    public override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        commonInit()
+    }
+    
+    public override init(nibName: String?, bundle: Bundle?) {
+        super.init(nibName: nibName, bundle: bundle)
+        commonInit()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        if #available(iOS 13.0, *) {
+            overrideUserInterfaceStyle = .dark
+        }
+    }
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            navigationBar.standardAppearance = appearance
+            navigationBar.scrollEdgeAppearance = appearance
+            navigationBar.compactAppearance = appearance
+            if #available(iOS 15.0, *) {
+                navigationBar.compactScrollEdgeAppearance = appearance
+            }
+        }
+    }
+}
+
 open class HXBaseViewController: UIViewController {
+    
+    public override init(nibName: String?, bundle: Bundle?) {
+        super.init(nibName: nibName, bundle: bundle)
+        commonInit()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        if #available(iOS 13.0, *) {
+            overrideUserInterfaceStyle = .dark
+        }
+    }
+    
     open override func viewDidLoad() {
         super.viewDidLoad()
         if #available(iOS 13.0, *) {
